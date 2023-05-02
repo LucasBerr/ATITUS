8c7ce95fdd0e730ec6ae7513c1ea992750cb068e
Lucas Tabelli Berr <68754446+LucasBerr@users.noreply.github.com>
2023-05-02 18:26:08 -0300

    Finishing Calendar.py
    Finishing code of Calendar.py

--- Calendar/Calendar.py
+++ Calendar/Calendar.py
@@ -1,15 +1,12 @@
 """
 Este programa faz um calendário no terminal utilizando o input do usuário
 Exemplo: 
-
 Informe o numero de dias do mes: 27
 VALOR INVALIDO! Informe o numero de dias do mes: 33
 VALOR INVALIDO! Informe o numero de dias do mes: 31
-
 Informe o dia da semana: 8
 VALOR INVALIDO! Informe o dia da semana: -1
 VALOR INVALIDO! Informe o dia da semana: 6
-
 DOM SEG TER QUA QUI SEX SAB
                     01  02
 03  04  05  06  07  08  09
@@ -20,21 +17,44 @@ DOM SEG TER QUA QUI SEX SAB
 """
 # Pegar o numero de dias do mês do usuário e verificar se é válido
 dias_do_mes = int(input("Informe o numero de dias do mês: "))
+# Criando flag dos dias do mês
+dias_validos = False
 while True: 
-    if dias_do_mes not in [28, 29, 30, 31]:
+    for i in range(28,32):
+        if dias_do_mes == i:
+            dias_validos = True
+    if dias_validos == False:
         dias_do_mes = int(input("VALOR INVÁLIDO! Informe o número de dias do mês: "))
-        if dias_do_mes in [28, 29, 30, 31]:
-            print()
+        for i in range(28, 32):
+            if dias_do_mes == i:
+                print(" ")
     else:
-        break
-
+        break        
+# Usando listas:  
+#while True:
+#   if dias_do_mes not in [28, 29, 30, 31]:
+#       dias_do_mes = int(input("VALOR INVÁLIDO! Informe o número de dias do mês: "))
+#       if dias_do_mes in [28, 29, 30, 31]:
+#           print()
+#   else:
+#       break
+    
 # Pegar o numero da semana na qual o mês começa e verificar se é válido
 dia_da_semana = int(input("Informe o dia da semana: "))
-while True:
-    if dia_da_semana not in [1, 2, 3, 4, 5, 6, 7]:
-        dia_da_semana = int(input("VALOR INVÀLIDO! Informe o dia da semana: "))
+while True: 
+    for i in range(1,8):
+        if dias_do_mes == i:
+            dias_validos = True
+    if dias_validos == False:
+        dias_do_mes = int(input("VALOR INVÀLIDO! Informe o dia da semana: "))
     else:
-        break
+        break 
+# Usando listas:  
+#while True:
+#    if dia_da_semana not in [1, 2, 3, 4, 5, 6, 7]:
+#        dia_da_semana = int(input("VALOR INVÀLIDO! Informe o dia da semana: "))
+#    else:
+#        break
 
 print()
 # Imprimir na tela os dias da semana
@@ -56,3 +76,4 @@ while dia <= dias_do_mes:
     else:
         print()
         max_semana = 1
+print("\n")

e8ae5d63b483ac946a0570606a527b196d988127
Lucas Berr <lucastberr@gmail.com>
2023-08-10 17:48:04 -0300

    Starting the venv so them i could install keyboard library. Correcting bugs from map.py

--- car_game/map.py
+++ car_game/map.py
@@ -1,28 +1,81 @@
 import keyboard
+import time
 
-class map:
+
+class Map:
     def __init__(self):
-        self.streetLeft =   "\t\t\tx"
+        self.streetLeft = "\t\t\tx"
         self.streetMiddle = "|"
-        self.streetRight =  "x"
+        self.streetRight = "x"
         self.blanckSpaces = "  "
 
-        self.interact = ["  ", "🚗", "🚙"] 
-        
+        self.interact = [["  ", "🚗", "🚙"], ["  ", "🚗", "🚙"], ["  ", "🚗", "🚙"], ["  ", "🚗", "🚙"]]
+        self.isPlayerHere = 0
+
         # x       |   🚗  x
 
-        self.window = 10
+        self.window = 20
 
     def construct_street(self):
         for i in range(self.window):
-             print(self.streetLeft + self.interact[0] + self.interact[0] + self.streetMiddle + self.interact[0] + self.interact[0] + self.streetRight)
+            print(
+                self.streetLeft + self.interact[0][0] + self.interact[1][0] + self.streetMiddle + self.interact[2][0] +
+                self.interact[3][0] + self.streetRight)
 
+    # I need to find a way to don't use so many ifs inside here
     def construct_street_with_player(self):
-            print(self.streetLeft + self.interact[0] + self.interact[1] + self.streetMiddle + self.interact[0] + self.interact[0] + self.streetRight)
 
-# 
+        for i in range(self.window):
+            if i != (self.window / 2):
+                print(
+                    self.streetLeft + self.interact[0][0] + self.interact[1][0] + self.streetMiddle + self.interact[2][
+                        0] + self.interact[3][0] + self.streetRight)
+            else:
+                if self.isPlayerHere == 0:
+                    print(
+                        self.streetLeft + self.interact[0][1] + self.interact[1][0] + self.streetMiddle +
+                        self.interact[2][
+                            0] + self.interact[3][0] + self.streetRight)
+                elif self.isPlayerHere == 1:
+                    print(
+                        self.streetLeft + self.interact[0][0] + self.interact[1][1] + self.streetMiddle +
+                        self.interact[2][
+                            0] + self.interact[3][0] + self.streetRight)
+                elif self.isPlayerHere == 2:
+                    print(
+                        self.streetLeft + self.interact[0][0] + self.interact[1][0] + self.streetMiddle +
+                        self.interact[2][
+                            1] + self.interact[3][0] + self.streetRight)
+                elif self.isPlayerHere == 3:
+                    print(
+                        self.streetLeft + self.interact[0][0] + self.interact[1][0] + self.streetMiddle +
+                        self.interact[2][
+                            0] + self.interact[3][1] + self.streetRight)
+
+    def move_right(self):
+        if not self.isPlayerHere >= 3:
+            self.isPlayerHere += 1
+
+    def move_left(self):
+        if not self.isPlayerHere <= 0:
+            self.isPlayerHere -= 1
 
 
-mapa = map()
-mapa.construct_street()
-mapa.construct_street_with_player()
+
+class Game:
+    def options(self):
+        # while keyboard.wait("esc"):
+        pass
+
+    def start_game(self, map: Map):
+        while not keyboard.is_pressed("esc"):
+            map.construct_street_with_player()
+            time.sleep(0.036)
+
+if __name__ == "__main__":
+    mapa = Map()
+    car_game = Game()
+
+    keyboard.add_hotkey("d", mapa.move_right)
+    keyboard.add_hotkey("a", mapa.move_left)
+    car_game.start_game(mapa)
